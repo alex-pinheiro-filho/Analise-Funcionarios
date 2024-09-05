2e482c6b8845b5f598d6ddcf17ad17f8ddaf1905
Alex <alexpinheirofilho.1997@gmail.com>
2024-09-05 13:34:02 -0300

    Segundo commit

--- script_rh.sql
+++ script_rh.sql
@@ -2,19 +2,19 @@ USE rh;
 SELECT * FROM datasetrh;
 
 
-SELECT 
-	   COUNT(Id_Funcionario) IS NULL AS Ids_Nulos,
-       COUNT(Id_Funcionario) AS Total_Registros,
-	   COUNT(DISTINCT Id_Funcionario) AS Total_Registros_Unicos
+SELECT
+	COUNT(Id_Funcionario) IS NULL AS Ids_Nulos,
+	COUNT(Id_Funcionario) AS Total_Registros,
+	COUNT(DISTINCT Id_Funcionario) AS Total_Registros_Unicos
 FROM datasetrh;
 
 -- ------------------------------------------------------------------------------------------------------
 -- MÉDIA DE SATISFAÇÃO POR FUNÇÃO, SALÁRIO POR FUNÇÃO E 10 MAIORES SALÁRIOS
 -- MÉDIA SATISFAÇÃO FUNÇÃO
 
-SELECT 
+SELECT
 	Funcao,
-    AVG(Nivel_Satisfacao_Trabalho) AS Media_satisfacao 
+	AVG(Nivel_Satisfacao_Trabalho) AS Media_satisfacao 
 FROM datasetrh
 GROUP BY Funcao
 ORDER BY Media_satisfacao DESC;
@@ -45,27 +45,27 @@ FROM datasetrh
 ORDER BY Indice_Envolvimento_Trabalho DESC;
 
 -- FUNCIONÁRIOS COM BAIXO ENVOLVIMENTO (NIVEL SATISFAÇÃO E SALÁRIO)
-SELECT 
+SELECT
 	Id_Funcionario,
-	Indice_Envolvimento_Trabalho,	
+	Indice_Envolvimento_Trabalho,
     Nivel_Satisfacao_Trabalho,
     Salario_Mensal
-FROM 
+FROM
 	datasetrh
 WHERE Indice_Envolvimento_Trabalho < 2
-ORDER BY 
+ORDER BY
 	Id_Funcionario;
 
 -- FUNCIONÁRIOS COM ENVOLVIMENTO BAIXO E SATISFAÇÃO ALTA
-SELECT 
+SELECT
 	Id_Funcionario,
     Salario_Mensal,
     Anos_na_Empresa,
     Funcao,
     Indice_Envolvimento_Trabalho,
-    Nivel_Satisfacao_Trabalho	
+    Nivel_Satisfacao_Trabalho
 FROM datasetrh
-WHERE 
+WHERE
 	Indice_Envolvimento_Trabalho = 1 AND
 	Nivel_Satisfacao_Trabalho = 4
 ORDER BY Id_Funcionario;
@@ -74,7 +74,7 @@ ORDER BY Id_Funcionario;
 -- SATISFAÇÃO E SALÁRIO POR ANOS DE EXPERIÊNCIA
 
 SELECT
-	CASE 
+	CASE
 		WHEN Anos_Experiencia < 1 THEN 'Aprendiz'
         		WHEN Anos_Experiencia <= 3 THEN 'Junior'
         		WHEN Anos_Experiencia <= 5 THEN 'Pleno'
@@ -91,7 +91,7 @@ ORDER BY Media_Salarial;
 -- ---------------------------------------------------------------
 -- FUNCIONÁRIOS COM TÍTULOS DE ANALISTA
 
-SELECT 
+SELECT
 	Funcao,
 	COUNT(*) AS Qtd_Funcionarios
 FROM datasetrh
@@ -102,7 +102,7 @@ ORDER BY Qtd_Funcionarios;
 -- ----------------------------------------------------------------
 -- FUNÇÃO MAIS FUNCIONÁRIOS (SATISFAÇÃO E MÉDIA SALARIAL) 
 
-SELECT 
+SELECT
     Funcao,
     Qtd_Funcionario,
     Nivel_Satisfacao_Trabalho,
@@ -110,14 +110,14 @@ SELECT
     Salarios_Abaixo_Media,
     ROUND(Salarios_Abaixo_Media / Qtd_Funcionario * 100, 2) AS Porcentagem_Salarios_Abaixo_Media
 FROM (
-    SELECT 
+    SELECT
         Funcao,
         COUNT(*) AS Qtd_Funcionario,
         ROUND(AVG(Nivel_Satisfacao_Trabalho), 2) AS Nivel_Satisfacao_Trabalho,
         ROUND(AVG(Salario_Mensal), 2) AS Media_Salario,
-        SUM(CASE 
-                WHEN Salario_Mensal < (SELECT AVG(Salario_Mensal) FROM datasetrh WHERE Funcao = d.Funcao) 
-                THEN 1 ELSE 0 
+        SUM(CASE
+                WHEN Salario_Mensal < (SELECT AVG(Salario_Mensal) FROM datasetrh WHERE Funcao = d.Funcao)
+                THEN 1 ELSE 0
             END) AS Salarios_Abaixo_Media
     FROM datasetrh d
     GROUP BY Funcao
@@ -127,7 +127,7 @@ ORDER BY Qtd_Funcionario DESC;
 -- --------------------------------------------------
 -- VARIAÇÃO DE SALÁRIO POR CATEGORIA DE PERFORMANCE
 
-SELECT 
+SELECT
     Aval_Performance AS Categoria_Avaliacao_Performance,
     count(*) AS Quantidade_Funcionarios,
     ROUND(AVG(Salario_Mensal), 2) AS Media_Salario
@@ -138,7 +138,7 @@ ORDER BY Media_Salario DESC;
 -- ----------------------------------------------------------------
 -- IMPACTO DOS TREINAMENTOS MO SALÁRIO E SATISFAÇÃO
 
-SELECT 
+SELECT
 	Numero_Treinamentos_Ano_Anterior AS Num_Treinamentos,
     ROUND(AVG(Salario_Mensal), 2) AS Media_Salarial,
     ROUND(AVG(Nivel_Satisfacao_Trabalho), 2) as Media_Satisfacao
@@ -149,7 +149,7 @@ ORDER BY Num_treinamentos;
 -- ----------------------------------------------------------------------------
 -- FUNCIONÁRIOS COM ALTA SATISFAÇÃO E DISPONÍVEIS HORA EXTRA
 
-SELECT 
+SELECT
 	Id_Funcionario,
     Nivel_Satisfacao_trabalho,
     Disponivel_Hora_extra
@@ -161,7 +161,7 @@ ORDER BY Id_Funcionario;
 -- ANÁLISE DE FUNCIONÁRIOS COM MENOS DE 5 ANOS DE EXPERIÊNCIA (QUANTIDADE E SALÁRIO MÉDIO)
 
 -- CONSULTA 1:
-SELECT 
+SELECT
     Id_Funcionario,
     Anos_Experiencia,
     Indice_Envolvimento_Trabalho,
@@ -171,7 +171,7 @@ FROM datasetrh
 WHERE Anos_Experiencia < 5;
 
 -- CONSULTA 2:
-SELECT 
+SELECT
 	COUNT(Id_Funcionario) AS Qtd_Funcionarios,
     ROUND(AVG(Salario_Mensal), 2) AS Media_Salarial
 FROM datasetrh
@@ -184,7 +184,7 @@ SELECT DISTINCT Indice_Envolvimento_Trabalho
 FROM datasetrh
 ORDER BY Indice_Envolvimento_Trabalho;
 
-SELECT 
+SELECT
 	CASE
 		WHEN Indice_Envolvimento_Trabalho = 1 THEN 'Baixo'
         WHEN Indice_Envolvimento_Trabalho > 1 AND Indice_Envolvimento_Trabalho < 4 THEN 'Medio'
